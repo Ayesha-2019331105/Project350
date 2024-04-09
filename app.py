8b72424b5bb4561c17b99d16cddb939ad20947ad
Ayesha-2019331105 <60940841+Ayesha-2019331105@users.noreply.github.com>
2024-04-09 16:37:54 +0600

    jkjlj

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
 from PIL import Image
 import io
 import os
 app = Flask(__name__, template_folder='templates')
+CORS(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,29 +22,27 @@ def index():
 
 @app.route('/fileupload', methods=['POST'])
 def file_upload():
-    file = request.files['file']
-    if file.content_type == 'text/plain':
-        return file.read().decode()
-    # Check if the uploaded file is an image
-    elif file.content_type.startswith('image/'):
-        # Read the image file
-        image_bytes = file.read()
-        # Create an in-memory file-like object
-        image_stream = io.BytesIO(image_bytes)
-        # Open the image using PIL (Python Imaging Library)
-        image = Image.open(image_stream)
-        # Display the image (you may need to render it in a template or use another method depending on your application)
-        image.show()
-        try:
-            os.mkdir('test/')
-        except:
-            pass
-        image.save('test/name01.png')
-        # Optionally, you can return a response to indicate that the image was processed successfully
-        return 'Image uploaded and displayed successfully'
-    else:
-        return ""
+    files = request.files.getlist("file")
+    for file in files:
+        if file.content_type == 'text/plain':
+            return file.read().decode()
+        # Check if the uploaded file is an image
+        elif file.content_type.startswith('image/'):
+            # Read the image file
+            image_bytes = file.read()
+            # Create an in-memory file-like object
+            image_stream = io.BytesIO(image_bytes)
+            # Open the image using PIL (Python Imaging Library)
+            image = Image.open(image_stream)
+            # Display the image (you may need to render it in a template or use another method depending on your application)
+            image.show()
+            try:
+                os.mkdir('test/')
+            except:
+                pass
+            image.save('test/'+file.filename)
+    return ""
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
